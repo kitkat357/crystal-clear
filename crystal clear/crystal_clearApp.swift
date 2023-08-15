1c1d8908492041a34373711c05b826af8bdae9e4
Rishika Jain <r1sh1kajain357@gmail.com>
2023-08-15 12:57:10 -0400

    Initial Commit

---
+++ crystal clear/crystal_clearApp.swift
@@ -0,0 +1,17 @@
+//
+//  crystal_clearApp.swift
+//  crystal clear
+//
+//  Created by rishika on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct crystal_clearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
